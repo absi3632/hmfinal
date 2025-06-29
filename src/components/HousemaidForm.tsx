c238f87205a4acaf62ede1f1b0c3f3d3fa7e4299
absi3632 <abd3632@gmail.com>
2025-06-29 23:13:06 +0300

    Fix unused imports in HousemaidForm

--- src/components/HousemaidForm.tsx
+++ src/components/HousemaidForm.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { 
   Save, 
-  X, 
   User, 
-  Mail, 
-  Phone, 
   MapPin, 
   Building, 
   Calendar, 
@@ -17,10 +14,9 @@ import {
   Users
 } from 'lucide-react';
 import { Housemaid } from '../types/housemaid';
-import { countries, passportCountries } from '../data/countries';
+import { passportCountries } from '../data/countries';
 import { generateHousemaidNumberIfEligible, shouldGenerateHousemaidNumber } from '../utils/localStorage';
 import { hasPermission, getCurrentUser } from '../utils/auth';
-import CountrySelect from './CountrySelect';
 import ProfilePhotoUpload from './ProfilePhotoUpload';
 import ProfilePhotoViewer from './ProfilePhotoViewer';
 import CVUpload from './CVUpload';
